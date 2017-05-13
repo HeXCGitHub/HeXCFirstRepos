82e3f086e0ce3fb914152487514d336120a668b5
gitboss <2054618234@qq.com>
2017-05-13 19:35:45 +0800

    commit demo01 secondly

--- demo01/src/com/demo/HelloGit.java
+++ demo01/src/com/demo/HelloGit.java
@@ -3,5 +3,6 @@ package com.demo;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("welcome to use git");
+		System.out.println("git is the best vcs");
 	}
 }
